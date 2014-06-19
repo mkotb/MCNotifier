7af7075cf4264500bba20a071979695bde74e269
Mazentheamazin <email@mazentheamazin.com>
2014-06-19 11:45:43 +0400

    Added API Class

--- src/io/mazenmc/notifier/Notifier.java
+++ src/io/mazenmc/notifier/Notifier.java
@@ -1,5 +1,31 @@
 package io.mazenmc.notifier;
 
+import io.mazenmc.notifier.client.NotifierClient;
+import io.mazenmc.notifier.event.NotifierEventHandler;
+import io.mazenmc.notifier.util.SettingsManager;
+
 public class Notifier {
-    //TODO: Create API Class
+    public static NotifierEventHandler getEventHandler() {
+        return NotifierPlugin.getEventHandler();
+    }
+
+    public static NotifierClient getClient(int id) {
+        return NotifierClient.getClient(id);
+    }
+
+    public static NotifierClient getClient(String username) {
+        return NotifierClient.getClient(username);
+    }
+
+    public static String[] generatePacketArgs(String... args) {
+        return args;
+    }
+
+    public static String[] emptyPacketArgs() {
+        return new String[] {};
+    }
+
+    public static SettingsManager getSettingsManager() {
+        return NotifierPlugin.getSettingsManager();
+    }
 }
