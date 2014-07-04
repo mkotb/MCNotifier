71a91a4e674b9f47ea07f30010776c52b9cd84c7
Mazentheamazin <email@mazentheamazin.com>
2014-07-04 14:14:55 +0400

    Renamed PacketPermissionRequest; added PacketPlayerPermRemove

---
+++ src/main/java/io/mazenmc/notifier/packets/PacketPlayerPermRemove.java
@@ -0,0 +1,34 @@
+package io.mazenmc.notifier.packets;
+
+import io.mazenmc.notifier.Notifier;
+import io.mazenmc.notifier.NotifierPlugin;
+import org.bukkit.entity.Player;
+
+public class PacketPlayerPermRemove extends Packet{
+
+    private static final int id = 16;
+    private String permission;
+    private Player permRequester;
+
+    public PacketPlayerPermRemove(String permission, Player permRequester) {
+        if(!Notifier.vaultDetected()) {
+            throw new IllegalStateException("Vault was not detected. Make sure Vault is enabled to utilize this feature.");
+        }
+        this.permission = permission;
+        this.permRequester = permRequester;
+    }
+
+    @Override
+    public void handle() {
+        NotifierPlugin.getPermission().playerRemove(permRequester, permission);
+    }
+
+    @Override
+    public String toString() {
+        return Notifier.buildString(id, SPLITTER, permRequester.getName(), SPLITTER, permission);
+    }
+
+    public static int getID() {
+        return id;
+    }
+}
--- src/main/java/io/mazenmc/notifier/packets/PacketPlayerPermissionAdd.java
+++ src/main/java/io/mazenmc/notifier/packets/PacketPlayerPermissionAdd.java
@@ -4,13 +4,13 @@ import io.mazenmc.notifier.Notifier;
 import io.mazenmc.notifier.NotifierPlugin;
 import org.bukkit.entity.Player;
 
-public class PacketPermissionRequest extends Packet{
+public class PacketPlayerPermissionAdd extends Packet{
 
     private static final int id = 15;
     private String permission;
     private Player permRequester;
 
-    public PacketPermissionRequest(String permission, Player permRequester) {
+    public PacketPlayerPermissionAdd(String permission, Player permRequester) {
         if(!Notifier.vaultDetected()) {
             throw new IllegalStateException("Vault was not detected. Make sure Vault is enabled to utilize this feature.");
         }
